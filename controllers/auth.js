a5c03ef05f6af22797afd5b7834c0550e61ce618
Eliecer David <eparialhuay@gmail.com>
2022-01-15 21:45:39 -0500

    add process to verify credentials in auth login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,22 @@
+const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 
-const login = (req, res = response) => {
+const User = require("../models/user");
+
+const login = async (req, res = response) => {
   const { email, password } = req.body;
+
+  const user = await User.findOne({ email })
+    .where(User.getActiveClause());
+
+  if (!user) {
+    return res.status(400).json({ message: "credentials are incorrect" });
+  }
+
+  const isValid = bcryptjs.compareSync(password, user.password);
+  if (!isValid) {
+    return res.status(400).json({ message: "credentials are incorrect" });
+  }
 
   res.json({
     message: "auth login from controller",
